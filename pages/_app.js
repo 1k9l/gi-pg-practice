d80212595ba89d4121c24c3ff98467e299e63a1d
1k9l <cyd.dahlia.art@gmail.com>
2025-01-30 21:58:31 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css';
+// File: pages/_app.js
+
+import Layout from '../components/Layout';
+import '../styles/globals.css'; // ensures .section, .result, etc. are loaded globally
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // Wrap every page in <Layout> so the navbar is always present
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
