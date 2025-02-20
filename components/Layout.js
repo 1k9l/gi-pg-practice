a2f715547762db980e2b3749a8d5c70e3ac673bc
1k9l <cyd.dahlia.art@gmail.com>
2025-02-19 19:00:52 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,13 +5,14 @@ import Link from 'next/link';
 export default function Layout({ children }) {
   return (
     <div>
-      {/* The shared navbar */}
+      {/* Shared Navbar */}
       <div className="navbar">
         <h1>Ordinal API Tester</h1>
         <div className="links">
           <Link href="/">Home</Link>
           <Link href="/connect" style={{ marginLeft: '1rem' }}>Connect</Link>
           <Link href="/maze" style={{ marginLeft: '1rem' }}>Maze</Link>
+          <Link href="/tap-demo" style={{ marginLeft: '1rem' }}>Tap Demo</Link>
         </div>
       </div>
 
